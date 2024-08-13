d0ea0d6a9c1df9906f06fa7f94000edfcad844b9
rizaru22 <rizaru.2.2@gmail.com>
2024-08-13 13:52:52 +0700

    belajar php hari ke-2

---
+++ harikedua/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$luas=7;  //deklarasi dan inisialisasi variabel 
+const phi=3.14; //deklarasi dan inisialisasi konstanta
+
+echo $luas;
+echo "<br>";
+echo phi;
+echo "<br>";
+
+$luasPersegiPanjang=67;//integer
+$kelilingLingkaran=93.5;
+$nama="Angga Saputra";
+$hidup=True;
+$luasPersegiPanjang="Enam Puluh Tujuh";
+
+var_dump($luasPersegiPanjang);
+var_dump($kelilingLingkaran);
+var_dump($nama);
+var_dump($hidup);
